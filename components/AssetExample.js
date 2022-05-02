99f74d3998677e88212c70b95b34d22a0619e338
Adam Goldfine <tulaneadam21@gmail.com>
2022-05-01 20:51:50 -0400

    Update AssetExample.js

--- components/AssetExample.js
+++ components/AssetExample.js
@@ -7,7 +7,7 @@ export default function AssetExample() {
       <Text style={styles.paragraph}>
         Local files and assets can be imported by dragging and dropping them into the editor
       </Text>
-      <Image style={styles.logo} source={require('../assets/snack-icon.png')} />
+      <Image style={styles.logo} source={require('../assets/icon.png')} />
     </View>
   );
 }
